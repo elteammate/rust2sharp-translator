10a97f7da6ce30812d7b2e0f08b16fedf2514d90
elteammate <elteammate@gmail.com>
2022-10-08 20:20:05 +0300

    Implement validator

--- PythonWebInterface/translator.py
+++ PythonWebInterface/translator.py
@@ -15,4 +15,35 @@ def translate(source: str) -> [str, str]:
 
 
 def validate(translated: str, expected: str) -> bool:
-    return translated == expected
+    i, j = 0, 0
+    tokens = {}
+
+    translated += "!EOF!"
+    expected += "!EOF!"
+
+    while i < len(translated) and j < len(expected):
+        if translated[i] == expected[j]:
+            i += 1
+            j += 1
+        elif translated[i].isspace():
+            i += 1
+        elif expected[j].isspace():
+            j += 1
+        elif expected[j] == "_":
+            token = ""
+            j += 1
+            while expected[j] != "_":
+                token += expected[j]
+                j += 1
+            j += 1
+            result_token = ""
+            while translated[i].isalnum():
+                result_token += translated[i]
+                i += 1
+            if token != "" and (token in tokens and tokens[token] != result_token or result_token in tokens.values()):
+                return False
+            tokens[token] = result_token
+        else:
+            return False
+
+    return True
