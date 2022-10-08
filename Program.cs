053362eb1ab0ace31a9c730599da510aca97ce5a
elteammate <elteammate@gmail.com>
2022-10-08 17:51:48 +0300

    Add console interface and Translator class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Rust2SharpTranslator;
 
-Console.WriteLine("Hello, World!");
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        string input;
+        try
+        {
+            input = File.ReadAllText(args[0]);
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Usage: <program> <input file> [output file]");
+            return -1;
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine("Input file does not exist: " + e.Message);   
+            return -1;
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Error reading file: " + e.Message);
+            return -1;
+        }
+
+        var output = Translator.Translate(input);
+
+        if (args.Length > 1)
+        {
+            try
+            {
+                File.WriteAllText(args[1], output);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error writing file: " + e.Message);
+                return -1;
+            }
+        }
+        else
+        {
+            Console.WriteLine(output);
+        }
+        
+        return 0;
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,9 @@
+namespace Rust2SharpTranslator;
+
+public static class Translator
+{
+    public static string Translate(string source)
+    {
+        return source;
+    }
+}
