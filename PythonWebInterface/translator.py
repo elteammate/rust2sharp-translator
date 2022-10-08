f1b6842618e1e1ffdc92f4cc411fcb2d3db4ce91
elteammate <elteammate@gmail.com>
2022-10-08 19:45:19 +0300

    Translate now invokes C# program

--- PythonWebInterface/translator.py
+++ PythonWebInterface/translator.py
@@ -1,5 +1,17 @@
+import os
+import subprocess
+
+
 def translate(source: str) -> [str, str]:
-    return source, "not implemented"
+    temp_file_source = "./temp/source.rs"
+    with open(temp_file_source, "w") as f:
+        f.write(source)
+
+    process = subprocess.run([os.getenv("TRANSLATOR_BINARY"), temp_file_source], capture_output=True)
+
+    result = process.stdout.decode("utf-8")
+    errors = process.stderr.decode("utf-8")
+    return result, errors
 
 
 def validate(translated: str, expected: str) -> bool:
